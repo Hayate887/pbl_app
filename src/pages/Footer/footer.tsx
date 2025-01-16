af908ebd0f7213c3316b1736cbfa120354aa828b
Hayate887 <sakusochisakusaku@gmail.com>
2025-01-16 15:22:57 +0900

    change footer

--- src/pages/Footer/footer.tsx
+++ src/pages/Footer/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <>
             <Flex direction="column">
                 <Box as="footer" bgGradient="linear(to-r, rgba(255,0,0,0.1), rgba(0,0,255,0.1))" py={4} textAlign="center" width="100%" minH={{ base: "10vh", md: "1vh" }}>
-                    <Text fontSize="sm" color="gray.600">Copyright (c) 2025 Hayate887</Text>
+                    <Text fontSize="sm" color="gray.600">Copyright &copy; 2025 Hayate887</Text>
                 </Box>
             </Flex>
         </>
